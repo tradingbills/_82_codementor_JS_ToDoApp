3e97ab754b154b64154d5b9fba03214ed8b1320c
Jason Ashley <tradingbills@gmail.com>
2018-05-08 08:49:11 -0700

    45:40 getter and setter on App Class' prototype with testing

--- session2/App.js
+++ session2/App.js
@@ -1,5 +1,13 @@
 const App = function() {
-  this.getState= ()=> { return{}};
+  this.state = {};
 };
 
+App.prototype.getState = function () { 
+  return this.state;
+};
+App.prototype.setState = function ( newState ) {
+  this.state= newState;
+}
+
 export default App;
+
--- session2/test/App.spec.js
+++ session2/test/App.spec.js
@@ -3,14 +3,29 @@ const expect= require('chai').expect;
 const should= require('chai').should();
 describe('App Class ' , () => {
 	it('Make an instance of the class App',()=>{
-		const instance = new App();
-		expect(instance).to.be.an.instanceof(App);
+		const app = new App();
+		expect(app).to.be.instanceof(App);
 	})
-	it('getState() method is an object on the App class', () => {
-		const instance2 = new App();
+	it('getState() method is an object added onto the App.protoype', () => {
+		const app2 = new App();
 		const expected = 'object';
-		const actual = typeof instance2.getState();
+		const actual = typeof app2.getState();
 		expect(actual).to.equal(expected);
+	})
+	it('checks usage of prototype on two instances using getState are the same', () => {
+		const app3 = new App();
+		const app4 = new App();
+		expect(app3.getState).to.equal(app4.getState)
+	})
+	it('sets a state', () => {
+		const newState = {};
+		const expected = newState;
+
+		const app5 = new App();
+		app5.setState( newState );
+		const actual= app5.getState();
+		
+		expect(actual).to.equal(expected)
 	})
 })
 	
